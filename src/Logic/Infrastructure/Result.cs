86c65e2ab205e753634375cac9c8068a53dd8159
mana8100 <emanacorda@outlook.com>
2023-10-22 17:04:31 -0400

    Added Managers for Cookies and Orders at Logic.

---
+++ src/Logic/Infrastructure/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace Logic.Infrastructure;
+
+public enum ResultErrorCode
+{
+    None,
+    IncompleteDozen,
+    EmptyOrder,
+    StoreError,
+    AlreadyExists
+}
+
+public class Result<T>
+{
+    public T? Data { get; init; } = default;
+
+    public bool Success { get; init; }
+
+    public ResultErrorCode Code { get; init; } = ResultErrorCode.None;
+
+    public string Error { get; init; } = string.Empty;
+}

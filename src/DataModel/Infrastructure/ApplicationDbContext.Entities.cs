eff918e20128f9bd1e34318f516e63f421246ccb
mana8100 <emanacorda@outlook.com>
2023-10-22 15:04:13 -0400

    Added DbContext to Data Model.

---
+++ src/DataModel/Infrastructure/ApplicationDbContext.Entities.cs
@@ -0,0 +1,29 @@
+﻿using DataModel.Model.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel.Infrastructure;
+
+public partial class ApplicationDbContext
+{
+    [ UsedImplicitly ]
+    public DbSet<Cookie> Cookies { get; set; } = null!;
+
+    [ UsedImplicitly ]
+    public DbSet<Order> Orders { get; set; } = null!;
+
+    [ UsedImplicitly ]
+    public DbSet<OrderItem> OrderItems { get; set; } = null!;
+
+    protected virtual void AddEntities( ModelBuilder builder )
+    {
+        builder.SetupCookieRelations();
+        builder.SetupCookieUniqueConstraints();
+
+        builder.SetupOrderRelations();
+        builder.SetupOrderUniqueConstraints();
+
+        builder.SetupOrderItemRelations();
+        builder.SetupOrderItemUniqueConstraints();
+    }
+}
